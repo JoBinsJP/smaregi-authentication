b1e0c099268bb89f965bf3f984d5a75f74772ab5
Ammar Shrestha <ammar@jobins.jp>
2020-12-22 10:51:37 +0900

    smaregi redirect url update

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ use Illuminate\Routing\Router;
 $router->group(
     ['middleware' => 'callback'],
     function (Router $router) {
-        $router->get(env('CALLBACK_URL', '/smaregi/auth/callback'), 'SmaregiAuthAction');
+        $router->get(env('SMAREGI_REDIRECT_URL', '/smaregi/auth/callback'), 'SmaregiAuthAction');
     }
 );
